cd92d6086be832cd5756e2fff561ed947a195350
José Manuel Alvarez <jalvarez@iMac-de-Jose.local>
2017-03-30 12:10:57 -0400

    Cargando estructura del proyecto con ejemplo de Shiny.

---
+++ src/Proyecto.r
@@ -0,0 +1,5 @@
+setwd("Documents/Proyecto-Mineria-de-Datos/")	# Cambiar directorio
+
+library(shiny)	# Se incluye la biblioteca de Shiny
+
+runApp("app", display.mode = "showcase")	# Se ejecuta la aplicación en Shiny
