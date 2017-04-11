bd0f96287030ed70e432d05158d8e24fef76b2ce
Jose Manuel <josemalvarezg1@gmail.com>
2017-04-10 21:59:56 -0400

    Actualizando pre-procesamiento de movies.

--- src/Proyecto.r
+++ src/Proyecto.r
@@ -19,6 +19,12 @@ ultimosCaracteres <- function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
 }
 
+#El titulo de cada pelicula va a ser tratado como un tipo de dato String (cadena de caracteres)
+movies$titulo_pelicula <- as.character(movies$titulo_pelicula)
+
+#Eliminando espacio al final del titulo de cada pelicula
+movies$titulo_pelicula <- gsub(") ", ")", movies$titulo_pelicula)
+
 #Se asigna a un nuevo vector "annos" los ultimos 6 caracteres de todas las celdas del titulo de cada pelicula
 annos <- ultimosCaracteres(as.character(movies$titulo_pelicula), 6)
 #Se elimina el ultimo caracter a cada año para eliminar el parentesis
@@ -29,11 +35,8 @@ annos <- substring(annos, 1, nchar(annos)-1)
 #Se agrega una nueva columna al dataset correspondiente al año de cada pelicula
 movies[ , "anno_pelicula"] <- annos
 
-#El titulo de cada pelicula va a ser tratado como un tipo de dato String (cadena de caracteres)
-movies$titulo_pelicula <- as.character(movies$titulo_pelicula)
-
 #Se eliminara el año del titulo de cada pelicula
-movies$titulo_pelicula <- substring(movies$titulo_pelicula, 1, nchar(movies$titulo_pelicula)-7)
+movies$titulo_pelicula <- substring(movies$titulo_pelicula, 1, nchar(movies$titulo_pelicula)-6)
 
 #Los generos de cada pelicula van a ser tratados como un tipo de dato String (cadena de caracteres)
 movies$generos <- as.character(movies$generos)
@@ -51,4 +54,9 @@ ratings$timestamp <- NULL
 
 #Se identifican las columnas del dataset leido
 colnames(ratings) <- c("ID_pelicula", "puntuacion" )
+
+
+alimentacion <- read.transactions("data/ratings.csv")
+reglas <- apriori(alimentacion)
+summary(reglas)
 
