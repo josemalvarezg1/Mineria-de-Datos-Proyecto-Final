11e19a803c1823a9667858992f4419c3e96670f0
Jose Manuel <josemalvarezg1@gmail.com>
2017-04-15 16:56:06 -0400

    Agregando lectura de dataset de links y mostrando imágenes de 4 películas recomendadas.

--- app/server.R
+++ app/server.R
@@ -2,6 +2,7 @@ library(shiny)  # Se incluye la biblioteca de Shiny
 library(arules) # Se incluye la biblioteca de Reglas de Asociacion
 library(arulesViz) # Se incluye la biblioteca de Reglas de Asociacion
 library(stringr) # Se incluye la biblioteca para manejar cadenas de caracteres
+library(jsonlite) # Se incluye la biblioteca para manejar JSON
 
 global <- reactiveValues()
 global$movies <- NULL
@@ -28,53 +29,115 @@ shinyServer(function(input, output, session) {
 
     if (input$selFilter == "Título") {
 
-      subreglas = subset(global$rulesByName, subset = lhs %pin% as.character(input$nameMovie))
+      subreglas = subset(global$rulesByName, subset = lhs %pin% paste("^",  as.character(input$nameMovie), "$", sep=""))
 
       if (length(subreglas) > 0) {
 
-        output$summary <- renderPrint({
+        #Validar cantidad        
         
-            #inspect(head(subreglas))  
+        inspect(head(subreglas))  
 
-            dfRules <- as(subreglas, "data.frame")
-            dfRules <- dfRules$rules
-            # Para mostrar el consecuente de la primera subregla
-            splitted <- as.list(strsplit(as.character(dfRules[1]), "=>")[[1]])
-            consequent <- as.character(splitted[2])
-            consequent <- substring(consequent, 3)
-            consequent <- substr(consequent, 1, nchar(consequent)-1) 
-            cat(consequent)   
-            cat("\n") 
-            # Para mostrar el consecuente de la segunda subregla
-            splitted <- as.list(strsplit(as.character(dfRules[2]), "=>")[[1]])
-            consequent <- as.character(splitted[2])
-            consequent <- substring(consequent, 3)
-            consequent <- substr(consequent, 1, nchar(consequent)-1) 
-            cat(consequent)   
-            cat("\n") 
-            # Para mostrar el consecuente de la tercera subregla
-            splitted <- as.list(strsplit(as.character(dfRules[3]), "=>")[[1]])
-            consequent <- as.character(splitted[2])
-            consequent <- substring(consequent, 3)
-            consequent <- substr(consequent, 1, nchar(consequent)-1) 
-            cat(consequent)   
-            cat("\n") 
-            # Para mostrar el consecuente de la cuarta subregla
-            splitted <- as.list(strsplit(as.character(dfRules[4]), "=>")[[1]])
-            consequent <- as.character(splitted[2])
-            consequent <- substring(consequent, 3)
-            consequent <- substr(consequent, 1, nchar(consequent)-1) 
-            cat(consequent)   
-            cat("\n") 
-            # Para mostrar el consecuente de la quinta subregla
-            splitted <- as.list(strsplit(as.character(dfRules[5]), "=>")[[1]])
-            consequent <- as.character(splitted[2])
-            consequent <- substring(consequent, 3)
-            consequent <- substr(consequent, 1, nchar(consequent)-1) 
-            cat(consequent)   
-            cat("\n") 
+        movies <- global$movies
+
+        dfRules <- as(subreglas, "data.frame")
+        dfRules <- dfRules$rules
+        # Para mostrar el consecuente de la primera subregla
+        splitted1 <- as.list(strsplit(as.character(dfRules[1]), "=>")[[1]])
+        consequent1 <- as.character(splitted1[2])
+        print(consequent1)
+        consequent1 <- substring(consequent1, 3)
+        consequent1 <- substr(consequent1, 1, nchar(consequent1)-1) 
+        consequent1 <- paste("^", consequent1, "$", sep="")
+        moviesSubset1 <- movies[grep(consequent1, movies$titulo_pelicula), ]
+        themoviedbId1 <- as.character(moviesSubset1[2])
+        print(consequent1)
+        print(themoviedbId1)
+
+
+        output$summary1 <- renderPrint({
+          
+          consequent1 <- substring(consequent1, 2)
+          consequent1 <- substr(consequent1, 1, nchar(consequent1)-1) 
+          cat(consequent1)   
 
         })
+
+        peliTemp1 <- fromJSON(paste("https://api.themoviedb.org/3/movie/", themoviedbId1, "?api_key=3c9a39604750b33b3e006c0d54a11e55", sep=""))
+        src1 <- paste0("https://image.tmdb.org/t/p/w500",peliTemp1$poster_path)
+        
+        output$poster1<-renderText({paste0('<img src="',src1,'" width="100%">')})
+
+        # Para mostrar el consecuente de la segunda subregla
+        splitted2 <- as.list(strsplit(as.character(dfRules[2]), "=>")[[1]])
+        consequent2 <- as.character(splitted2[2])
+        consequent2 <- substring(consequent2, 3)
+        consequent2 <- substr(consequent2, 1, nchar(consequent2)-1) 
+        consequent2 <- paste("^", consequent2, "$", sep="")
+        moviesSubset2 <- movies[grep(consequent2, movies$titulo_pelicula), ]
+        themoviedbId2 <- as.character(moviesSubset2[2])
+        print(consequent2)
+        print(themoviedbId2)
+
+        output$summary2 <- renderPrint({
+        
+          consequent2 <- substring(consequent2, 2)
+          consequent2 <- substr(consequent2, 1, nchar(consequent2)-1) 
+          cat(consequent2)   
+
+        })
+
+        peliTemp2 <- fromJSON(paste("https://api.themoviedb.org/3/movie/", themoviedbId2, "?api_key=3c9a39604750b33b3e006c0d54a11e55", sep=""))
+        src2 <- paste0("https://image.tmdb.org/t/p/w500",peliTemp2$poster_path)
+        
+        output$poster2<-renderText({paste0('<img src="',src2,'" width="100%">')})
+        
+        # Para mostrar el consecuente de la tercera subregla
+        splitted3 <- as.list(strsplit(as.character(dfRules[3]), "=>")[[1]])
+        consequent3 <- as.character(splitted3[2])
+        consequent3 <- substring(consequent3, 3)
+        consequent3 <- substr(consequent3, 1, nchar(consequent3)-1) 
+        consequent3 <- paste("^", consequent3, "$", sep="")
+        moviesSubset3 <- movies[grep(consequent3, movies$titulo_pelicula), ]
+        themoviedbId3 <- as.character(moviesSubset3[2])
+        print(consequent3)
+        print(themoviedbId3)
+
+        output$summary3 <- renderPrint({
+        
+          consequent3 <- substring(consequent3, 2)
+          consequent3 <- substr(consequent3, 1, nchar(consequent3)-1) 
+          cat(consequent3)   
+
+        })
+
+        peliTemp3 <- fromJSON(paste("https://api.themoviedb.org/3/movie/", themoviedbId3, "?api_key=3c9a39604750b33b3e006c0d54a11e55", sep=""))
+        src3 <- paste0("https://image.tmdb.org/t/p/w500",peliTemp3$poster_path)
+        
+        output$poster3<-renderText({paste0('<img src="',src3,'" width="100%">')})
+        
+        # Para mostrar el consecuente de la cuarta subregla
+        splitted4 <- as.list(strsplit(as.character(dfRules[4]), "=>")[[1]])
+        consequent4 <- as.character(splitted4[2])
+        consequent4 <- substring(consequent4, 3)
+        consequent4 <- substr(consequent4, 1, nchar(consequent4)-1) 
+        consequent4 <- paste("^", consequent4, "$", sep="")
+        moviesSubset4 <- movies[grep(consequent4, movies$titulo_pelicula), ]
+        themoviedbId4 <- as.character(moviesSubset4[2])
+        print(consequent4)
+        print(themoviedbId4)
+
+        output$summary4 <- renderPrint({
+        
+          consequent4 <- substring(consequent4, 2)
+          consequent4 <- substr(consequent4, 1, nchar(consequent4)-1) 
+          cat(consequent4)   
+
+        })                        
+
+        peliTemp4 <- fromJSON(paste("https://api.themoviedb.org/3/movie/", themoviedbId4, "?api_key=3c9a39604750b33b3e006c0d54a11e55", sep=""))
+        src4 <- paste0("https://image.tmdb.org/t/p/w500",peliTemp4$poster_path)
+        
+        output$poster4<-renderText({paste0('<img src="',src4,'" width="100%">')})
 
       } else {
 
@@ -132,6 +195,8 @@ shinyServer(function(input, output, session) {
   })
 
   observeEvent(input$preProcess, {
+
+    ######################################################################################################
 
     inFile <- input$fileMovies
     if (is.null(inFile)) return(NULL) 
@@ -201,7 +266,51 @@ shinyServer(function(input, output, session) {
     # Eliminar variable generos
     movies_aux$generos <- NULL
 
+    ######################################################################################################
+
+    inFile <- input$fileLinks
+    if (is.null(inFile)) return(NULL) 
+
+    if (input$includeheaderlinks) {
+        h <- TRUE
+    } else {
+        h <- FALSE
+    }
+   
+    if (input$radioseparatorlinks=="commalinks") {
+        separador <- ","
+    } else if(input$radioseparatorlinks=="semicolonlinks") {
+        separador <- ";"
+    } else if(input$radioseparatorlinks=="tablinks") {
+        separador <- "\t"
+    }
+
+    #print(separador)
+
+    if(input$radioquotlinks=="doublelinks"){
+        comilla <- "\""
+    }else if(input$radioquotlinks=="simplelinks"){
+        comilla <- "\'"
+    }else if(input$radioquotlinks=="nonelinks"){
+        comilla <- ""
+    }
+
+
+    #Se lee el dataset de peliculas
+    links <- read.csv(inFile$datapath, header=h, sep=separador,quote = comilla)
+
+    #Se elimina el id de imdb
+    links$imdbId <- NULL
+
+    #Se identifican las columnas del dataset leido
+    colnames(links) <- c("ID_pelicula", "tmdbId")
+
+    #Se hace el merge de links y movies segun el id de cada pelicula
+    movies_aux <- merge(links,movies_aux,by="ID_pelicula")
+
     global$movies <- movies_aux
+
+    ######################################################################################################
 
     inFile <- input$fileRatings
     if (is.null(inFile)) return(NULL)  
@@ -258,23 +367,19 @@ shinyServer(function(input, output, session) {
 
     #Reglas de asociacion
 
-    # Se transforman las columnas a Factor para poder cambiar el dataframe a transactions
+    #Reglas por titulo de pelicula
     userrates <- merge(global$ratings,global$movies,by="ID_pelicula")
     userrates2 = as( split(as.vector(userrates$titulo_pelicula), as.vector(userrates$userId)) , "transactions" )
     rules = apriori( userrates2 , parameter=list(supp=0.005, conf=0.5, target="rules", minlen=2, maxlen=2,maxtime=20) )
-
     rules = sort(rules, by ="lift")
-    subreglas = subset(rules, subset = lhs %pin% as.character("Angels & Demons"))
 
     global$rulesByName <- rules
 
-
+    #Reglas por anno de cada pelicula
     userrates <- merge(global$ratings,global$movies,by="ID_pelicula")
     userrates2 = as( split(as.vector(userrates$anno_pelicula), as.vector(userrates$userId)) , "transactions" )
     rules = apriori( userrates2 , parameter=list(supp=0.005, conf=0.5, target="rules", minlen=2, maxlen=2,maxtime=20) )
-
     rules = sort(rules, by ="lift")
-    subreglas = subset(rules, subset = lhs %pin% as.character("1980"))
 
     global$rulesByYear <- rules
 
